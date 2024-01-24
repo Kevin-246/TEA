9312bb1b1f8743aa2ebecda543e79510e1198cf3
Kevin <kevincheng.nrp@gmail.com>
2024-01-23 21:32:48 -0800

    Initial Commit

---
+++ TinyEncryption/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TinyEncryption
+//
+//  Created by CodingKevin on 2024-01-23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
